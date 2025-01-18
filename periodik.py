cd037825db88c929c61247d0ac1c09dd41a161bd
Kyza18 <dzakykyzaaa@gmail.com>
2025-01-19 01:21:52 +0700

    Update periodik.py

--- periodik.py
+++ periodik.py
@@ -176,7 +176,7 @@ elif st.session_state["page"] == "table":
             st.markdown(f'<div class="card card-green"><strong>Simbol Unsur:</strong> {data["simbol"]}</div>', unsafe_allow_html=True)
             st.markdown(f'<div class="card card-orange"><strong>Nomor Atom:</strong> {data["nomor_atom"]}</div>', unsafe_allow_html=True)
             st.markdown(f'<div class="card card-blue"><strong>Nomor Massa:</strong> {data["nomor_massa"]}</div>', unsafe_allow_html=True)
-            st.success('This is a success!', icon="🔥🔥🔥")
+            st.success('This is a success!', icon="🔥")
         else:
             st.error("Unsur tidak ditemukan. Harap masukkan nama unsur yang benar.")
         
