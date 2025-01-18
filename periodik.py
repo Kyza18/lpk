2613478a6c8a9f009bd6436056030477ffdb0b75
Kyza18 <dzakykyzaaa@gmail.com>
2025-01-19 02:30:48 +0700

    Update periodik.py

--- periodik.py
+++ periodik.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import time
 
+pages = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
 # Fungsi untuk menambahkan gaya kustom
 def add_custom_styles():
     st.markdown("""
