94707439b23d12614d24e3b10c60baedbe84b498
Kyza18 <dzakykyzaaa@gmail.com>
2025-01-19 03:04:54 +0700

    Update periodik.py

--- periodik.py
+++ periodik.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
+st.image("https://images.unsplash.com/photo-1735447814306-8887e953a91f?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
 # Fungsi untuk menambahkan gaya kustom
 def add_custom_styles():
     st.markdown("""
